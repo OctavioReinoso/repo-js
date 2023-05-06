b16fe8bf313dc6c34e82160980219c84ebffde58
Octavio Reinoso <oct.reinoso@gmail.com>
2023-05-05 21:26:35 -0300

    nuevo

--- app.js
+++ app.js
@@ -8,59 +8,91 @@ productos.push(new Producto("Zapatatillas adidas", "Lona", 35000, "no"));
 productos.push(new Producto("Zapatillas nike", "Goma", 30000, "si"));
 
 carrito = [];
+
 let opcion;
 let elegirProducto;
-const contProductos = productos.map((prod)=>prod.titulo+ "\n");
+// const contProductos = productos.map((prod) => prod.titulo + "\n");
+// const carritoTotal = carrito.map((carritoProd) => carritoProd + "");
+let agregar = "";
+let seguir;
 
-do{
-opcion = Number(prompt("Seleccione alguna de las opciones que desee ingresar\n 1-Mostrar todos los productos\n 2-Buscar algún producto por su nombre\n 3-Agregar un producto\n 4-Vaciar todo"));
 
-switch (opcion) {
-    case 1:
-        mostrarTodo();
-        break;
-    case 2:
-        mostrarProducto();
-        // mostrarNombre();
-        break;
-    case 3:
-        agregarProd();
-        break;
-    default:
-        alert("Opcion invalida");
-}
+do {
+    opcion = Number(prompt("Seleccione alguna de las opciones que desee ingresar\n 1-Mostrar todos los productos\n 2-Buscar algún producto por su nombre\n 3-Agregar un producto\n 4-Quitar algun producto del carrito"));
 
-}while(opcion <= 5) 
+    switch (opcion) {
+        case 1:
+            mostrarTodo();
+            break;
+        case 2:
+            mostrarProducto();
+            break;
+        case 3:
+            agregarProd();
+            break;
+        case 4:
+            quitarProd();
+            break;
+        default:
+            alert("Opcion invalida");
+    }
 
-function mostrarTodo(){
-    alert(contProductos);
+} while (opcion <= 5)
+
+function mostrarTodo() {
+    const todoProductos = productos.map((producto) => {
+        alert(producto.titulo);
+    });
 }
 
 function mostrarProducto() {
     const inputUsuario = prompt("Ingrese la prenda que desea ver");
     productos.forEach((producto) => {
         if (producto.titulo === inputUsuario) {
-            alert(producto.titulo  +"\nPrecio: "+producto.precio +"\nMaterial: "+producto.material +"\nCosto de envio?: "+producto.costoEnvio);
+            alert(producto.titulo + "\nPrecio: " + producto.precio + "\nMaterial: " + producto.material + "\nCosto de envio?: " + producto.costoEnvio);
         }
     });
 }
 
-function agregarProd(){
-    const inputUsuario = prompt("Que producto desea agregar?");
-    productos.forEach((producto) =>{
-        if(producto.titulo === inputUsuario){
-            carrito = productos.push(inputUsuario);
-        }
-        alert("Producto agregado " + carrito);
-    });
+function agregarProd() {
+    let inputUsuario = prompt("Que producto desea agregar?");
+    const agregarMas = productos.find((items)=>items.titulo === inputUsuario);
+    carrito.push(agregarMas);
+    seguirSiNo();
+    console.log("Productos agregados: " + inputUsuario);
 }
 
-function quitarProd(){
-    const inputUsuario = Number(prompt("Ingrese con un numero el producto que desee eliminar"));
+function quitarProd() {
+    console.log(carrito);
+    const inputUsuario = Number(prompt("Seleccione con un numero los productos que desee eliminar según el orden agregado?")); 
+    // if (inputUsuario == 1) {
+    //     carrito.splice(0, 1);
+    // }
+    switch(inputUsuario){
+        case 1:
+            carrito.splice(0, 1);
+            break;
+        case 2:
+            carrito.splice(1, 1);
+            break;
+        case 3:
+            carrito.splice(2, 1);
+        default:
+            alert("gracias");
+    }
     
+    console.log("Productos en el carrito: " + carrito);
 }
 
-
+function seguirSiNo() {
+    seguir = prompt("Desea seguir agregando productos? s/n");
+    if (seguir == "s") {
+        agregarProd();
+    }
+    else {
+        alert("Volviendo al inicio");
+    }
+}
 
 
 
--- producto.js
+++ producto.js
@@ -24,19 +24,3 @@ class Producto{
 }
 
 
-// productos.push(new Producto("Remera blanca", "Algodón", 8000, "si"));
-// productos.push(new Producto("Remera negra", "Algodon", 8500, "si"));
-// productos.push(new Producto("Pantalon tipo jean", "Tela", 15000, "si"));
-// productos.push(new Producto("Pantalon jogger", "Tela", 10000, "no"));
-// productos.push(new Producto("Zapatatillas adidas", "Lona", 35000, "no"));
-// productos.push(new Producto("Zapatillas nike", "Goma", 30000, "si"));
-
-
-
-// const productos = [
-    //     {titulo:"Remera blanca", material: "Algodon", precio: 8000, costoEnvio: "si"},
-    //     {titulo:"Remera negra", material: "Algodon", precio: 8500, costoEnvio: "si"},
-    //     {titulo:"Pantalon tipo jean", material: "Tela", precio: 15000, costoEnvio: "si"},
-    //     {titulo:"Pantalon jogger", material: "Tela", precio: 10000, costoEnvio: "no"},
-    //     {titulo:"Zapatatilla adidas", material: "Lona", precio: 35000, costoEnvio: "no"},
-    //     {titulo:"Zapatilla nike", material: "Goma", precio: 30000, costoEnvio: "si"}];
